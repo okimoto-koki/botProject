af141890e38ef9cba4545bfb7356ae980bd17db4
kota777 <kota777jp@icloud.com>
2014-09-02 15:25:56 +0900

    read_DB.rbを新しいツイートだけ取得するように更新

--- DB_IO_from_console/read_DB.rb
+++ DB_IO_from_console/read_DB.rb
@@ -5,13 +5,37 @@
 
 require 'mysql2'
 
+# DBに接続
 client = Mysql2::Client.new(:host => 'localhost', :username => 'root', :password => 'root', :database => 'reudy_test')
+
 while true
-  client.query('SELECT TML_ID, USR_ID, MESSAGE FROM T_TIME_LINE').each do |data|
-    # p id,name,text
-    puts "TML_ID: #{data['TML_ID']}, USR_ID:#{data['USR_ID']}, MESSAGE:#{data['MESSAGE']}"
+  # 取ってきたものを表示
+
+  client.query('SELECT TML_ID, USR_ID, MESSAGE FROM T_TIME_LINE order by TML_ID DESC LIMIT 1').each do |data|
+
+    # 最終ツイートIDを取ってくる
+    lastIDread = open('../lastID.dat', 'r')
+    lastID = lastIDread.gets.to_i
+    lastIDread.close
+    p lastID
+
+    # 最終ツイートIDが最新と一緒、もしくは最新ユーザIDが自分なら飛ばす
+    if lastID == data['TML_ID']
+      puts ('same TML_ID or my tweet')
+    else
+
+      # 上で無ければ表示
+      puts "TML_ID: #{data['TML_ID']}, USR_ID:#{data['USR_ID']}, MESSAGE:#{data['MESSAGE']}"
+    end
+
+    # lastID.datを更新
+    lastIDwrite = open("../lastID.dat", "w")
+    lastIDwrite.puts data['TML_ID']
+    lastIDwrite.close
+    puts 'ここまで'
+
   end
-  puts 'ここまで'
+  # 5秒に一回ループさせる
   sleep 5
 end
 
